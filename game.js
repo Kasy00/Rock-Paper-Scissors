a8803a6b10ad3b0bfcdf6ceea71ed5813d943d9b
Kasy00 <kamilsyktus3@gmail.com>
2023-08-18 18:34:17 +0200

    Add playRound() function

--- game.js
+++ game.js
@@ -6,4 +6,40 @@ function getComputerChoice()
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerChoice)
+{
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection == "rock" && computerChoice == "paper")
+    {
+        console.log("You Lose! Paper beats Rock!");
+    }
+    
+    else if(playerSelection == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors!");
+    }
+
+    else if(playerSelection == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock!");
+    }
+
+    else if(playerSelection == "paper" && computerChoice == "scissors"){
+        console.log("You Lose! Scissors beats Paper!");
+    }
+
+    else if(playerSelection == "scissors" && computerChoice == "rock"){
+        console.log("You Lose! Rock beats Scissors!");
+    }
+
+    else if(playerSelection == "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats Paper!");
+    }
+
+    else{
+        console.log("It's a draw!");
+    }
+}
+
+const playerSelection = prompt("Enter your choice: ");
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerSelection, computerChoice));
